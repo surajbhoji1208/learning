27497e39d173ceb5dd9606ee63fd92b38b40c8d9
Suraj Bhoji <surajbhoji1208@gmail.com>
2026-03-12 09:14:31 +0530

    update

--- javaScript/app.js
+++ javaScript/app.js
@@ -18,3 +18,27 @@ Function.prototype.myBind =function(...args){
 
 const myBindemethod = printName.myBind({fistName:"suraj"})
 myBindemethod()
+
+
+
+
+//===============================Retry with promise========================================
+function attemptRetry(fn,retry,delay)
+{
+    return new Promise((resolve, reject)=>{
+        function attempt(remaining)
+        {
+            fn.then(resolve)
+            .catch(err=>{
+                if(remaining ==0) return reject(err)
+                
+                setTimeout(()=>{
+                    attempt(remaining-1)
+                },delay)
+            })
+        }
+        attempt(retry)
+    })
+}
+
+attemptRetry(fetchData,3,1000)
